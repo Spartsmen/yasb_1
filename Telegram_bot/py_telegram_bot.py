25233b4335183b53f878e7e8778cc3ae6c969fb7
Spartsmen <aapyzhov56@gmail.com>
2023-09-20 16:52:47 +0300

    logging into db

--- Telegram_bot/py_telegram_bot.py
+++ Telegram_bot/py_telegram_bot.py
@@ -20,11 +20,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                                    text=Message_texts.GREETING)
 
 cursor.execute('SELECT telegram_id FROM chats WHERE username = "client"')
-Client_id = cursor.fetchone()[0]
+Client_group_id = cursor.fetchone()[0]
 cursor.execute('SELECT telegram_id FROM chats WHERE username = "support"')
-Support_id = cursor.fetchone()[0]
+Support_group_id = cursor.fetchone()[0]
 
-async def save_message_to_db_client(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+async def save_message_to_db(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     user_id = user.id
     username = user.username
@@ -38,19 +39,24 @@ async def save_message_to_db_client(update: Update, context: ContextTypes.DEFAUL
     message_id = update.message.message_id
     if 'reply_to' in context.user_data and update.message.reply_to_message:
         chat_id, message_id = context.user_data['reply_to']
-        first_message_id = context.user_data.get('first_message_id')
-        await context.bot.send_message(chat_id=Client_id, text=update.message.text,reply_to_message_id=first_message_id)
+        first_message_id = context.user_data.get(f'first_message_id_{user_id}')
+        await context.bot.send_message(chat_id=Client_group_id, text=update.message.text,reply_to_message_id=first_message_id)
         del context.user_data['reply_to']
     elif update.message.text.startswith('@yasb_testing_bot'):
-        context.user_data['first_message_id'] = update.message.message_id
+        user_id = update.effective_user.id
+        context.user_data[f'first_message_id_{user_id}'] = update.message.message_id
         keyboard = [[InlineKeyboardButton('Reply', callback_data=f'reply_{chat_id}_{message_id}')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        forwarded_message = await context.bot.send_message(chat_id=Support_id,
+        forwarded_message = await context.bot.send_message(chat_id=Support_group_id,
                                                            text=(f'''{update.effective_user.username} is asking -
-{update.message.text}'''), reply_markup=reply_markup)
+{update.message.text[19:]}'''), reply_markup=reply_markup)
+
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Your message has been sent')
-    else:
-        pass
+    ticket_number = context.user_data.get('ticket_number')
+    message_text = update.message.text
+    cursor.execute("INSERT INTO messages (ticket_id, text) VALUES (?, ?)",
+                       (ticket_number, message_text))
+    conn.commit()
 
 
 async def handle_reply_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -58,9 +64,16 @@ async def handle_reply_button(update: Update, context: ContextTypes.DEFAULT_TYPE
     await query.answer()
     chat_id, message_id = query.data.split('_')[1:]
     context.user_data['reply_to'] = (chat_id, message_id)
-    await context.bot.send_message(chat_id=Support_id, text=f'{update.effective_user.username} is replying....',
+    await context.bot.send_message(chat_id=Support_group_id, text=f'{update.effective_user.username} is replying....',
                                    reply_markup=ForceReply())
     await query.edit_message_reply_markup()
+    client_id = update.effective_user.id
+    supporter_id = query.from_user.id
+    cursor.execute("INSERT OR IGNORE INTO tickets (chat_id,client_id, helper_id) VALUES (?, ?, ?)",
+                   (Client_group_id, client_id, supporter_id))
+    conn.commit()
+    cursor.execute(f'SELECT id FROM tickets WHERE client_id ={client_id}')
+    context.user_data['ticket_number'] = cursor.fetchone()[0]
 
 
 load_dotenv()
@@ -71,7 +84,7 @@ if __name__ == '__main__':
 
     start_handler = CommandHandler('Start', start)
     application.add_handler(start_handler)
-    db_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, save_message_to_db_client)
+    db_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, save_message_to_db)
     application.add_handler(db_handler)
     reply_handler = CallbackQueryHandler(handle_reply_button, pattern='^reply_')
     application.add_handler(reply_handler)
